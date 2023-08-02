a2c7e497f6c45f91e13f8e1082315b25ead8dea7
Babureddymarolix <babumarolix@gmail.com>
2023-08-02 20:50:50 +0530

    nyt out

---
+++ program/nyt.py
@@ -0,0 +1,49 @@
+text = "i am the your the good"
+print(len(text))
+x=text.split("the")
+print(x)
+print(len(x))
+
+
+s="marolix technology solutions"
+splt =s.split()
+for x in splt:
+    print(x)
+
+
+s="my name is babu"
+print(len(s))
+splt=s.split()
+print(len(splt))
+for x in splt:
+    print(x)
+
+
+date="02-08-23"
+split=date.split(":")
+for x in split:
+    print(x)
+
+
+lst =["babu","nani","hemanth"]
+s="+".join(lst)
+print(sorted)  
+
+
+tpl=("me","you","we")
+print(len(tpl))
+s1 ="*".join(tpl)
+print(len(s1))
+print(s1)
+
+
+s="learning python"
+print(s.upper())
+
+
+s="ms dhoni is my hero"
+print(s.lower())
+
+
+s2="ms dhoni is my hero"
+print(s2.capitalize())
