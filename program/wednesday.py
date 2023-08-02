145b7c7ee153d96df39e58dacb69560a5cf1642e
Babureddymarolix <babumarolix@gmail.com>
2023-08-02 10:20:03 +0530

    assign

--- program/wednesday.py
+++ program/wednesday.py
@@ -1,5 +1,41 @@
 s="i want learn python"
 print(s.rfind("a"))
 
+s="i love india"
+print(s.rfind("e"))
+
+s="mahendra sign dhoni"
+print(s.lfind("o"))
+
+s="mahendra shing dhoni"
+print(s.rfind("n"))
+
+s="we are all indians"
+print(s.rfind("a"))
+
+s="i want learn python"
+print(s.rfind("a"))
+
+s="abababababababababaabaaabbb"
+print(s.count("a"))
 
 
+s=input("main string:")
+sub=input("sub string:")
+if s.index(sub):
+    print(sub,"is found")
+ else:
+    print(sub,"is not found")   
+
+
+ a="babu"
+ b="reddy"
+print(a+b) 
+
+a="babu"
+print(a*4)
+
+
+
+s=" learning python is very easy"
+print[1:8:1]
