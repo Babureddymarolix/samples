41ebd00fa87086d92af32d39f25dda9afc52bc1b
Babureddymarolix <babumarolix@gmail.com>
2023-08-04 09:27:32 +0530

    friday

---
+++ program/friday.py
@@ -0,0 +1,18 @@
+string = input("enter string:")
+if(string.end switch("gmail.com")):
+    print("user can signin")
+else:
+    print("gmail is not verified")
+
+
+string=input("enter  string:")
+if(string.endswitch("www.")):
+    print("show user interface")
+else:
+    print("website not found")
+
+s="babu 06191"
+print(s.isalnum())
+
+s2="Babu"
+print(s2.isalpha())
