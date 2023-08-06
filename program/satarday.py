8f55545aac8f341021f0a2444a63dc4180a5c6b4
Babureddymarolix <babumarolix@gmail.com>
2023-08-06 11:02:39 +0530

    nithya

--- program/satarday.py
+++ program/satarday.py
@@ -1,21 +1,17 @@
-def remove_characters(input_string, characters_to_remove):
-    # Create an empty string to store the result
-    result = ""
+def is_palindrome(string):
+    # Remove any spaces and convert the string to lowercase
+    string = string.replace(" ", "").lower()
+    # Compare the string with its reverse
+    return string == string[::-1]
 
-    # Loop through each character in the input string
-    for char in input_string:
-        # Check if the character is not in the characters_to_remove list
-        if char not in characters_to_remove:
-            # If not, add it to the result string
-            result += char
+if __name__ == "__main__":
+    user_input = input("Enter a string: ")
 
-    return result
+    if is_palindrome(user_input):
+        print("The string is a palindrome.")
+    else:
+        print("The string is not a palindrome.")
 
-# Example usage:
-input_string = "Hello, World!"
-characters_to_remove = " ,!"  # We want to remove spaces, commas, and exclamation marks
 
-result_string = remove_characters(input_string, characters_to_remove)
-print(result_string)  # Output will be "HelloWorld"
 
 
