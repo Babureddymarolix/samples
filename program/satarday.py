80401b2ac9828cf4638bfe6b7dd3c2397ba8543c
Babureddymarolix <babumarolix@gmail.com>
2023-08-06 11:11:03 +0530

    nani

--- program/satarday.py
+++ program/satarday.py
@@ -6,7 +6,6 @@ def is_palindrome(string):
 
 if __name__ == "__main__":
     user_input = input("Enter a string: ")
-
     if is_palindrome(user_input):
         print("The string is a palindrome.")
     else:
