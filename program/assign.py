06861dd3f0c33d34ed9e8ae13d1a02262e9b49d6
Babureddymarolix <babumarolix@gmail.com>
2023-08-07 17:39:29 +0530

    assign

--- program/assign.py
+++ program/assign.py
@@ -25,7 +25,7 @@ print(s2)
 
 
 
-s="aaabbbccc1112223333"
+s="aabbcc112233**"
 digitc=0
 alphac=0
 speciac=0
@@ -36,10 +36,34 @@ for i in s:
         alphac=alphac+1
     else:
         specialc=alphac+1
-print("number of special characters",specialc)
+print("number of special",specialc)
 print("number of digit",digitc) 
-print("number of digit",alphac)               
+print("number of alpha",alphac)  
 
+
+
+
+s=input("enter string:")
+s2=s.replace("","")
+print(s2)
+
+
+str=input("enter a string:")
+sum=0
+for i in str:
+  if i.isdigit():
+    sum=sum+int(i)
+print("sum=",sum)  
+
+
+s=["babu","nani","chaitu","chaitu","hemmanth"]
+s=list(dict.fromkeys(s))
+print(s)
+
+
+string="i love india"
+s=string.count("i")
+print(s)
 
 
 
