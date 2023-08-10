828848609d035b17372cfff705609084d9b96fb5
Babureddymarolix <babumarolix@gmail.com>
2023-08-10 10:19:48 +0530

    08

---
+++ program/08.py
@@ -0,0 +1,4 @@
+mylist=[10,20,30,40,50,51]
+s="babu"
+l=list(s)
+print(l)
--- program/tuesday.py
+++ program/tuesday.py
@@ -3,3 +3,11 @@ salary="20000"
 age=24
 print("{a} salary is {b} and his age is {c}". format(a=name,b=salary,c=age))
 
+
+vowels="a,e,i,o,u,A,E,I,O,U"
+ch=input("enter char:")
+if (ch=="a", or ch=="e", or ch=="i", or ch=="o", ch=="u", or ch=="A", or ch=="E", or ch=="I", or ch=="O", or ch=="U"):
+    print("given char",ch,"is vowel")
+else:
+    print("given char",ch,"is not vowel")
+
