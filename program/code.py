93694256c58e18e4e198fa73e25d0f19e43f326b
Babureddymarolix <babumarolix@gmail.com>
2023-08-23 16:55:19 +0530

    dict

--- program/code.py
+++ program/code.py
@@ -2,8 +2,55 @@ mytuple=[10,20,30,40,50,60]
 print(type(mytuple))
 
 
+mytuple=[10,20,30,40,50,60]
+print(type(mytuple))
+
+
 t=10,20,30,40,50
 print(type(t))
+
+
+num=1,2,3,4,5
+t1=tuple(num)
+print(type(t1))
+print(t1)
+
+
+num=1,2,3,4,5
+t1=tuple(num)
+print(tuple(t1))
+print(t1)
+
+
+tp1=(2,4,6,8,10)
+print(tp1[2])
+
+
+tp1=(2,4,6,8,10)
+print(tp1[3:4+1])
+
+
+myteam=["ravi","babu","navya","praveen","nani"]
+x=list(myteam)
+print(x)
+x[3]="hemanth"
+print(x)
+y=tuple(x)
+print(y)
+
+
+t1=("python","java")
+t2=("devops",)
+print(type(t2))
+t1+t2
+print(t1)
+
+
+t1=("python","java")
+t2=("devops",)
+print(type(t2))
+t1+=t2
+print(t1)
 
 
 num=1,2,3,4,5
