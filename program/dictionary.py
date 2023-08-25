38505eecce2a22227f58353b7bd3b047b8858fe8
Babureddymarolix <babumarolix@gmail.com>
2023-08-25 13:21:18 +0530

    hello

--- program/dictionary.py
+++ program/dictionary.py
@@ -1,56 +1,12 @@
-dict={0:10,1:20}
-print(dict)
-dict.update({2:30})
-print(dict)
-
-d={}
-for k in range(0,5):
-    key=input("enter keys")
-    value=input("enter values")
-    d[key]=value
-    print(d)
+num={'physics','maths','chemistry'}
+print(list(num[1]))
 
 
-d1=dict()
-for x in range(1,16):
- d1[x]=x**2
- print(d1)   
-     
-dict={1:10,2:20,3:30,4:40,5:50,6:60}
-def is_key_present(x):
-   if x in d:
-      print("key is present in the dictionary")
-   else:
-      print("key is not present in the dictionary ")
-      is_key_present(5)
-      is_key_present(9)    
-     
-
-dic1={'data1':10,'data2':20,'data3':30}
-print(sum(dic1.values()))     
-
-dic1={'data1':10,'data2':20,'data3':30}
-sum=0
-for i in dic1.values():
-   sum=sum+i
-   print(sum)
-   print()
+num={'physics':30,'maths':40,'chemistry':50}
+print(list(num[2]))
 
 
-   d={}
-   for k in range(0,5):
-      key=input("enter keys:")
-      value=input("enter values:")
-      d[key]=value
-      print(d)
-      remove_key=input("enetr removed key")
-      d.pop(remove_key)
-      print(d)
-
-
-d1={'a':100,'b':200}
-d2={'x':300,'y':200}
-d=d1.copy()
-d.update(d2)
-print(d)      
-   
+dict1={'a':10,'b':20,'c':30}
+dict2={'b':5,'c':15,'d':25}
+combined=combine_dictionaries(dict1,dict2)
+print(combined)
