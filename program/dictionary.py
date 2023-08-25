d0ecab9f5f41c88025c010cc9b30b9cf5a289a85
Babureddymarolix <babumarolix@gmail.com>
2023-08-25 10:55:54 +0530

    hello

--- program/dictionary.py
+++ program/dictionary.py
@@ -1,3 +1,8 @@
+dict={0:10,1:20}
+print(dict)
+dict.update({2:30})
+print(dict)
+
 d={}
 for k in range(0,5):
     key=input("enter keys")
@@ -11,5 +16,5 @@ for x in range(1,16):
  d1[x]=x**2
  print(d1)   
      
-      
+di      
      
