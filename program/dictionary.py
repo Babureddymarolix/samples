58ecccf48429e132029a0531b85ad35463e44401
Babureddymarolix <babumarolix@gmail.com>
2023-08-25 10:49:11 +0530

    dict

--- program/dictionary.py
+++ program/dictionary.py
@@ -1,17 +1,15 @@
-dict={0:10,1:20}
-print(dict)
-dict.update({2:30})
-print(dict)
+d={}
+for k in range(0,5):
+    key=input("enter keys")
+    value=input("enter values")
+    d[key]=value
+    print(d)
 
-d={1:10,2:20,3:30,4:40,5:50,6:60}
-def is_key_present(x):
-  if x in d:
-        print("key is present in the dictionary")
-  else:
-      print("key is not present in the dictionary")
-      is_key_present(5)
-      is_key_present(8)
 
+d1=dict()
+for x in range(1,16):
+ d1[x]=x**2
+ print(d1)   
      
       
      
