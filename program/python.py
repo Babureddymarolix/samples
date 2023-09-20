82e839e47b4d7031f3c6df22ea87598c3c71c901
Babureddymarolix <babumarolix@gmail.com>
2023-09-20 09:35:04 +0530

    user

---
+++ program/python.py
@@ -0,0 +1,16 @@
+def employee_details():
+    d = {}
+    employ_entry = int(input("Enter the number of employes: "))
+    for i in range(employ_entry):
+        emp_age = input("Enter employee age: ")
+        emp_domain = input("Enter domain: ")
+    if emp_domain in d:
+        d[emp_domain].append[emp_age]
+    else:
+      d[emp_domain]=emp_age
+    print("Employee details:")
+    print("*****************")
+    for domain, name in d.items():
+        print("Domain:", domain)
+        print("Employee Names:", emp_age)
+employee_details()
