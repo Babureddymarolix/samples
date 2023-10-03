7b149f28ac48a5643ec6a686378826b3aebbbcb4
Babureddymarolix <babumarolix@gmail.com>
2023-10-03 09:48:20 +0530

    calc

---
+++ program/calculator.py
@@ -0,0 +1,28 @@
+print('two numbers below:')
+a=int(input('enter first number:'))
+b=int(input('enter second number:'))
+chr==0
+while chr<4:
+    print('calculator menu')
+    print('1.add')
+    print('2.substact')
+    print('3.multiply')
+    print('4.divide')
+    chr==int(input('enter ur choice:'))
+    if chr==1:
+        c=a+b
+        print('sum:',c)
+    elif chr==2:
+      c=a-b
+      print('difference:',c)
+    elif chr==3:
+        c=a*b
+        print('product:',c)
+    elif chr==4:
+         c=a/b
+         print('quoient:',c)
+    else:
+        print('invalid choice')
+
+      
+       
