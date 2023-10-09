35589d8004084c1b21c577accc3515dcffea8ac9
Babureddymarolix <babumarolix@gmail.com>
2023-10-09 10:23:04 +0530

    oops

---
+++ program/oops.py
@@ -0,0 +1,20 @@
+class employee:
+    company="marolix"
+    def _init_(self,name,id):
+     self.name=Name
+     self.id=id
+    e1=employee("babu",101)
+    e2=employee("nani",102)
+
+
+
+
+class employee:
+    company="marolix"
+    def _init_(self):
+     self.name="babu"
+     employee.od=101
+     def m1(self):
+       self.mail="babu@gmail.com"
+       employee.salay=2500
+       e1=employee()
