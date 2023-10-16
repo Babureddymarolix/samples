60ab5ee129a6a36d010a13c58ad0f8dca0aafd7c
Babureddymarolix <babumarolix@gmail.com>
2023-10-16 10:01:57 +0530

    store

---
+++ program/shop.py
@@ -0,0 +1,16 @@
+num1=5
+num2=3
+result=num1+num2
+print("the result of addition is :",result)
+
+
+num1=10
+num2=4
+result=num1-num2
+print("the result of subtraction is:",result)
+
+
+num1=12
+num2=3
+result=num1/num2
+print("the result of division is:",result)
