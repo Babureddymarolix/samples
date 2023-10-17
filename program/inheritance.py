9e4405dd2c39828961f34fe66e7fe270609ac811
Babureddymarolix <babumarolix@gmail.com>
2023-10-17 22:13:24 +0530

    multilevel

--- program/inheritance.py
+++ program/inheritance.py
@@ -1,20 +1,31 @@
-class Parent:
-    def _init_(self, name):
-        self.name = name
+class Grandparent:
+    def _init_(self, grandparent_name):
+        self.grandparent_name = grandparent_name
 
-    def show_name(self):
-        print("Name:", self.name)
+    def show_grandparent_name(self):
+        print("Grandparent Name:", self.grandparent_name)
+
+class Parent(Grandparent):
+    def _init_(self, grandparent_name, parent_name):
+        super()._init_(grandparent_name)
+        self.parent_name = parent_name
+
+    def show_parent_name(self):
+        print("Parent Name:", self.parent_name)
 
 
 class Child(Parent):
-    def _init_(self, name, age):
-        super()._init_(name)
-        self.age = age
+    def _init_(self, grandparent_name, parent_name, child_name):
+        super()._init_(grandparent_name, parent_name)
+        self.child_name = child_name
 
-    def show_age(self):
-        print("Age:", self.age)
+    def show_child_name(self):
+        print("Child Name:", self.child_name)
 
-child = Child("babu",25)
 
-child.show_name()  
-child.show_age()   
+child = Child("Grandma", "Mom", "Alice")
+
+
+child.show_grandparent_name() 
+child.show_parent_name()      
+child.show_child_name()        
