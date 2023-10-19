a76ab0bf6389198f85e15647eb4e2760e6c975b5
Babureddymarolix <babumarolix@gmail.com>
2023-10-19 09:14:02 +0530

    oops

--- program/inheritance.py
+++ program/inheritance.py
@@ -28,4 +28,49 @@ child = Child("Grandma", "Mom", "Alice")
 
 child.show_grandparent_name() 
 child.show_parent_name()      
-child.show_child_name()        
+child.show_child_name()   
+
+
+
+
+class Parent1:
+    def method1(self):
+        print("Method from Parent1")
+
+class Parent2:
+    def method2(self):
+        print("Method from Parent2")
+
+class Child(Parent1, Parent2):
+    def child_method(self):
+        print("Method from Child")
+
+child = Child()
+child.method1()  
+child.method2() 
+
+child.child_method()  
+
+
+class Parent:
+    def method(self):
+        print("Method from Parent")
+
+class Child1(Parent):
+    def child1_method(self):
+        print("Method from Child1")
+
+
+class Child2(Parent):
+    def child2_method(self):
+        print("Method from Child2")
+
+child1 = Child1()
+child2 = Child2()
+
+
+child1.method()        
+child1.child1_method()  
+
+child2.method()        
+child2.child2_method()  
