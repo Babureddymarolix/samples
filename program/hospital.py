e8f462de8bbeb6eff5be7620d039f960d65fb242
Babureddymarolix <babumarolix@gmail.com>
2023-10-26 12:06:39 +0530

    medicine

--- program/hospital.py
+++ program/hospital.py
@@ -23,3 +23,22 @@ def display_patient_details(patients):
             print(f"Address: {patient['address']}")
             print(f"Phone: {patient['phone']}")
             print() 
+
+
+
+patient1 = {'id': 1, 'name': 'John', 'age': 30, 'gender': 'Male', 'address': '123 Main St', 'phone': '555-1234'}
+patient2 = {'id': 2, 'name': 'Alice', 'age': 25, 'gender': 'Female', 'address': '456 Elm St', 'phone': '555-5678'}
+patient3 = {'id': 3, 'name': 'Bob', 'age': 40, 'gender': 'Male', 'address': '789 Oak St', 'phone': '555-9012'}
+
+patient_list = [patient1, patient2, patient3]
+
+search_id = 2  
+found_patient = search_patient_by_id(patient_list, search_id)
+
+if found_patient:
+    print("Patient found:")
+    print(f"Name: {found_patient['name']}")
+    print(f"Age: {found_patient['age']}")
+ 
+else:
+    print(f"No patient found with ID {search_id}")
