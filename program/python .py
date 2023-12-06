9559e1e60c0a4f22f9f6eeb301a3c7e5add82d15
Babureddymarolix <babumarolix@gmail.com>
2023-12-06 13:09:52 +0530

    pyth

---
+++ program/python .py
@@ -0,0 +1,28 @@
+arr=[1,2,3,4,5]
+print(sum(arr))   
+
+
+arr=[1,2,3,4,5]
+max=arr[0]
+n=len(arr)
+for i in range(1,n):
+    if arr[i]>max:
+        max=arr[i]
+print(max)        
+
+
+arr=[1,2,3,4,5]
+min=arr[0]
+n=len(arr)
+for i in range(1,n):
+    if arr[i]>min:
+        min=arr[i]
+print(min)
+
+
+
+
+
+mylist=[1,23,5,6,77,7]
+print(mylist)
+print("length of list using len()method is:",len(mylist))
