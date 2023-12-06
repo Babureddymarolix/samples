0c82f93735a0dd7ef676097f3b189a520ace47a8
Babureddymarolix <babumarolix@gmail.com>
2023-12-06 13:40:08 +0530

    pyth

--- program/python .py
+++ program/python .py
@@ -26,3 +26,8 @@ print(min)
 mylist=[1,23,5,6,77,7]
 print(mylist)
 print("length of list using len()method is:",len(mylist))
+
+
+mylistlist=[3,545,65,67]
+mylist[0],mylist[-1]=mylist[-1],mylist[0]
+print("list after swapping:",mylist)
