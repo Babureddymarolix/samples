0d75bb150ba6c2567082372286d069759b37284d
Babureddymarolix <babumarolix@gmail.com>
2023-12-14 09:38:16 +0530

    practise

--- program/14.py
+++ program/14.py
@@ -1,25 +1,20 @@
-str="reverse me"
-print(str)
-print(str[::-1])
-
-
-
-s=input("enter palindrome:")
-if(s==s[::-1]):
-    print("yes it is palindrome")
-else:
-    print("no it is palindrome")
-
-
-
 def remove_vowels(string):
     vowels="aeiouAEIOU"
     result=""
-for char in string:
-    if char not in vowels:
+    for char in string:
+      if char not in vowels:
         result +=char
+    return result
 print(remove_vowels("hello world"))
 
 
 
 
+lst=["red","black","green","yellow","blue"]
+lst.pop(4)
+print(lst)
+
+
+mylst={"1","1","2","2","3","4","5","1"}
+s=lst(dict.fromkeys(mylst))
+print(s)
