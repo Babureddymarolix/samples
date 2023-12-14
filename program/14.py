326a6f8d21a824776c062c0457471ecf7251a7b6
Babureddymarolix <babumarolix@gmail.com>
2023-12-14 09:52:58 +0530

    practise

--- program/14.py
+++ program/14.py
@@ -1,20 +1,35 @@
-def remove_vowels(string):
-    vowels="aeiouAEIOU"
-    result=""
-    for char in string:
-      if char not in vowels:
-        result +=char
-    return result
-print(remove_vowels("hello world"))
+a=[1,2,3,5,6,7,8,2]
+count={}
+for i in a:
+    if i in count:
+        count[i]+=1
+    else:
+        count[i]=1
+    print(count)
+
+
+
+a=[10,20,30,40,20,30,20,101,20,20,20,10]
+dup_items=set()
+uniq_items=[]
+for x in a:
+    if x not in dup_items:
+        uniq_items.append(x)
+        dup_items.add(x)
+    print(dup_items)
+
+
+
+l=[1,2,3,4,5,6,4]
+print(l.pop())
+print(l)
 
 
 
 
-lst=["red","black","green","yellow","blue"]
-lst.pop(4)
-print(lst)
-
-
-mylst={"1","1","2","2","3","4","5","1"}
-s=lst(dict.fromkeys(mylst))
-print(s)
+list=[1,2,3,4,5,7,1,2,3,4,5,5]
+result=[1,2,3,4,5,7]
+for i in list:
+    if i not in result:
+        result.append(i)
+    print(result)
