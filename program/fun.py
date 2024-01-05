1002e1c8052462fb8e3905a1eda1a64b4a9611f9
Babureddymarolix <babumarolix@gmail.com>
2024-01-05 09:57:01 +0530

    fun

--- program/fun.py
+++ program/fun.py
@@ -8,6 +8,20 @@ num2=temp
 
 
 
+num=6
+count=0
+if num>1:
+    for i in range (1,num+1):
+        if (num%i)==0:
+           count=count+1
+if count==2:
+    print("number is prime")
+else:
+    print("number is not prime")
+
+
+
+
 
 
 
