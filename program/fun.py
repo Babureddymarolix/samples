524844f7507d4a6494672cf2cd18332ac4a7d9ea
Babureddymarolix <babumarolix@gmail.com>
2024-01-05 09:45:47 +0530

    fun

--- program/fun.py
+++ program/fun.py
@@ -1,16 +1,23 @@
-class solution(object):
-    def delvowels(self,s):
-        s=s.replace("a","")
-        s=s.replace("e","")
-        s=s.replace("i","")
-        s=s.replace("o","")
-        s=s.replace("u","")
-        return s
-    chr=solution()
-print(chr.delvowels(input("enter string:")))
+ch=input("please enter your own character:")
+if (ch>='0' and ch<='9'):
+    print("the given character",ch,"is a digit")
+else:
+    print("the given character",ch,"is not a digit")
+
+
+str=input("enter a input:")
+vowels=['a','e','i','o','u','A','E','I','O','u']
+if str in vowels:
+    print("the given char is vowels")
+else:
+    print("the given char is not vowels")
 
 
 
 
 
 
+
+
+ 
+ 
